f499fa2687ebcfc7da69ac60d36757ca020646bc
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2022-03-29 19:09:29 +0900

    Add: vue-smooth-scroll

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+import vueSmoothScroll from "vue-smooth-scroll";
 
 import("./assets/css/style.css");
 import("./assets/css/animate.min.css");
 
 Vue.config.productionTip = false;
+Vue.directive("smooth-scroll", vueSmoothScroll);
+Vue.use(vueSmoothScroll);
 
 /* eslint-disable no-new */
 new Vue({
