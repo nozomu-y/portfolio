22aa4f90ade2920988c9321558cfe7410d5f82d7
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2022-04-03 21:22:15 +0900

    Update: format

--- src/App.vue
+++ src/App.vue
@@ -14,26 +14,26 @@ export default {
   name: "App",
   components: {
     Nav,
-    Footer,
+    Footer
   },
   methods: {
-    createPageTitle: function (to) {
+    createPageTitle: function(to) {
       if (to.meta.title) {
         var setTitle = to.meta.title + " | Nozomu's Portfolio";
         document.title = setTitle;
       } else {
         document.title = "Nozomu's Portfolio";
       }
-    },
+    }
   },
-  mounted: function () {
+  mounted: function() {
     var to = this.$route;
     this.createPageTitle(to);
   },
   watch: {
     $route(to, from) {
       this.createPageTitle(to);
-    },
-  },
+    }
+  }
 };
 </script>
