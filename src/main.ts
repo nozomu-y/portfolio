7659b477bf10c809ead002e86886423323417e21
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-01-29 20:30:15 +0900

    Add: font awesome

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import "./style.css";
 import router from "./router";
 import App from "./App.vue";
 
+/* fontawesome */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+library.add(faBars);
+
 const app = createApp(App);
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
