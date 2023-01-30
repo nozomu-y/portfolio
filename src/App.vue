af741dd09340425601e0d514c152bee419584d0a
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-01-30 10:05:13 +0900

    Update: page template and routes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <div class="navbar bg-base-300 shadow mb-8">
+        <div class="flex-1">
+            <router-link to="/" class="btn btn-ghost normal-case text-xl">Nozomu's Portfolio</router-link>
+        </div>
+        <div class="hidden md:block flex-none">
+            <ul class="menu menu-horizontal px-1">
+                <li><router-link to="/">Home</router-link></li>
+                <li>
+                    <router-link to="/portfolio">Portfolio</router-link>
+                </li>
+                <li>
+                    <router-link to="/skills">Skills</router-link>
+                </li>
+            </ul>
+        </div>
+        <div class="flex-none md:hidden">
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <font-awesome-icon icon="fa-solid fa-bars" />
+                </label>
+                <ul tabindex="0"
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><router-link to="/">Home</router-link></li>
+                    <li>
+                        <router-link to="/portfolio">Portfolio</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/skills">Skills</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <router-view />
+
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content mt-10">
+        <div>
+            <p>Copyright © 2023 - Nozomu Yamazaki</p>
+        </div>
+    </footer>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./pages/Home.vue";
+import Portfolio from "./pages/Portfolio.vue";
+import Skills from "./pages/Skills.vue";
 
 const routes = [
-    { path: '/', name: 'helloworld', component: HelloWorld },
-]
+    { path: "/", name: "home", component: Home },
+    { path: "/portfolio", name: "portfolio", component: Portfolio },
+    { path: "/skills", name: "skills", component: Skills },
+];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
-})
+});
 
 export default router;
