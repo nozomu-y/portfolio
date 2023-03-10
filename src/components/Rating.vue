e25b41eaf6b69ff0eb9a637bb8c25f3f4018f900
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-03-10 15:35:00 +0900

    Add: components

---
+++ src/components/Rating.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="rating">
+    <input
+      type="radio"
+      name="rating-star-2"
+      class="rating-hidden"
+      :checked="rating == '0'"
+    />
+    <input
+      type="radio"
+      name="rating-2"
+      class="mask mask-star-2 bg-blue-400"
+      :checked="rating == '1'"
+    />
+    <input
+      type="radio"
+      name="rating-2"
+      class="mask mask-star-2 bg-blue-400"
+      :checked="rating == '2'"
+    />
+    <input
+      type="radio"
+      name="rating-2"
+      class="mask mask-star-2 bg-blue-400"
+      :checked="rating == '3'"
+    />
+    <input
+      type="radio"
+      name="rating-2"
+      class="mask mask-star-2 bg-blue-400"
+      :checked="rating == '4'"
+    />
+    <input
+      type="radio"
+      name="rating-2"
+      class="mask mask-star-2 bg-blue-400"
+      :checked="rating == '5'"
+    />
+  </div>
+  {{ rating }}
+</template>
+
+<script lang="ts">
+export default {
+  props: ["rating"],
+};
+</script>
---
+++ src/components/Skill.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="not-prose bg-base-100 rounded-box w-full max-w-lg shadow-xl p-4 pt-4 pb-4 mb-3"
+  >
+    <div class="flex items-center gap-4 my-2">
+      <div class="avatar flex-none">
+        <div
+          class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"
+        >
+          <img
+            :src="logo_url"
+            width="94"
+            height="94"
+            :alt="logo_alt"
+            class="mask mask-squircle bg-white"
+          />
+        </div>
+      </div>
+      <div class="flex-auto">
+        <div class="text-lg font-extrabold">{{ language }}</div>
+        <div class="text-base-content/70 text-sm">{{ experience }}</div>
+      </div>
+      <div class="flex-auto">
+        <Rating :rating="rating"></Rating>
+      </div>
+    </div>
+    <hr class="my-3" />
+    <slot name="description"></slot>
+    <slot name="badges"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Rating from "./Rating.vue";
+
+export default {
+  components: {
+    Rating,
+  },
+  props: ["language", "experience", "rating", "logo_url", "logo_alt"],
+};
+</script>
