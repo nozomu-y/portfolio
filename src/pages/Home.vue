ac73dccf8369e37375dcf823dccc08851a5fd32c
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-03-14 15:54:16 +0900

    Update: make the Home layout responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,180 +17,186 @@
       </ul>
       <h2>Employment</h2>
 
-      <div
-        class="not-prose bg-base-100 rounded-box w-full max-w-lg shadow-xl p-4 pt-4 pb-4 mb-3"
-      >
-        <div class="flex gap-4 my-2">
-          <div class="avatar flex-none">
-            <div
-              class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"
-            >
-              <img
-                src="https://media.licdn.com/dms/image/C560BAQFHnDwDl15Pzg/company-logo_200_200/0/1624521003692?e=1683158400&v=beta&t=qeiHfqw-BqtlrQAiWLcL8_eu05jCWKP-KOuJoiBin74"
-                width="94"
-                height="94"
-                alt="LINE Corp Logo"
-                class="mask mask-squircle"
-              />
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div
+          class="not-prose bg-base-100 rounded-box w-full shadow-xl p-4 pt-4 pb-4"
+        >
+          <div class="flex gap-4 my-2 items-center">
+            <div class="avatar flex-none">
+              <div
+                class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"
+              >
+                <img
+                  src="https://media.licdn.com/dms/image/C560BAQFHnDwDl15Pzg/company-logo_200_200/0/1624521003692?e=1683158400&v=beta&t=qeiHfqw-BqtlrQAiWLcL8_eu05jCWKP-KOuJoiBin74"
+                  width="94"
+                  height="94"
+                  alt="LINE Corp Logo"
+                  class="mask mask-squircle"
+                />
+              </div>
+            </div>
+            <div class="flex-1">
+              <div class="text-lg font-extrabold">LINE Corp</div>
+              <div class="text-base-content/70 text-sm">
+                Software Engineer – Internship
+              </div>
+              <div class="text-base-content/70 text-sm">
+                September 2022 – October 2022
+              </div>
+              <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
             </div>
           </div>
-          <div class="flex-1">
-            <div class="text-lg font-extrabold">LINE Corp</div>
-            <div class="text-base-content/70 text-sm">
-              Software Engineer – Internship
-            </div>
-            <div class="text-base-content/70 text-sm">
-              September 2022 – October 2022
-            </div>
-            <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
+          <hr class="my-3" />
+          <div class="prose text-sm mb-3">
+            <strong>Responsibility:</strong>
+            <p>
+              Web application development and API development for LINE NEWS, as
+              well as backend system development.
+            </p>
+            <strong>Achievement:</strong>
+            <ul class="list-disc list-inside">
+              <li>
+                Constructed an automated infrastructure for load testing and
+                scenario generation.
+              </li>
+              <li>
+                Migrated the content management system for LINE NEWS and
+                improved the UI.
+              </li>
+            </ul>
+            <p class="mt-2">
+              <a
+                href="https://engineering.linecorp.com/ja/blog/automated-load-testing-scenario"
+                target="_blank"
+                class="link"
+                >Internship Report (Japanese)</a
+              >
+            </p>
+          </div>
+          <div>
+            <div class="badge badge-ghost mr-1">Java</div>
+            <div class="badge badge-ghost mr-1">Spring Boot</div>
+            <div class="badge badge-ghost mr-1">JavaScript</div>
+            <div class="badge badge-ghost mr-1">Vue.js</div>
           </div>
         </div>
-        <hr class="my-3" />
-        <div class="prose text-sm mb-3">
-          <strong>Responsibility:</strong>
-          <p>
-            Web application development and API development for LINE NEWS, as
-            well as backend system development.
-          </p>
-          <strong>Achievement:</strong>
-          <ul class="list-disc list-inside">
-            <li>
-              Constructed an automated infrastructure for load testing and
-              scenario generation.
-            </li>
-            <li>
-              Migrated the content management system for LINE NEWS and improved
-              the UI.
-            </li>
-          </ul>
-          <p class="mt-2">
-            <a
-              href="https://engineering.linecorp.com/ja/blog/automated-load-testing-scenario"
-              target="_blank"
-              class="link"
-              >Internship Report (Japanese)</a
-            >
-          </p>
-        </div>
-        <div>
-          <div class="badge badge-ghost mr-1">Java</div>
-          <div class="badge badge-ghost mr-1">Spring Boot</div>
-          <div class="badge badge-ghost mr-1">JavaScript</div>
-          <div class="badge badge-ghost mr-1">Vue.js</div>
-        </div>
-      </div>
 
-      <div
-        class="not-prose bg-base-100 rounded-box w-full max-w-lg shadow-xl p-4 pt-4 pb-4 mb-3"
-      >
-        <div class="flex gap-4 my-2">
-          <div class="avatar flex-none">
-            <div
-              class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"
-            >
-              <img
-                src="https://media.licdn.com/dms/image/C4E0BAQFL4lU6JYVm0A/company-logo_200_200/0/1532071807421?e=1683158400&v=beta&t=64iE9RODq89eRFoG3vXd8wJFrQeKSHNzjt4jl1G6M2g"
-                width="94"
-                height="94"
-                alt="Cookpad Logo"
-                class="mask mask-squircle"
-              />
+        <div
+          class="not-prose bg-base-100 rounded-box w-full shadow-xl p-4 pt-4 pb-4"
+        >
+          <div class="flex gap-4 my-2 items-center">
+            <div class="avatar flex-none">
+              <div
+                class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"
+              >
+                <img
+                  src="https://media.licdn.com/dms/image/C4E0BAQFL4lU6JYVm0A/company-logo_200_200/0/1532071807421?e=1683158400&v=beta&t=64iE9RODq89eRFoG3vXd8wJFrQeKSHNzjt4jl1G6M2g"
+                  width="94"
+                  height="94"
+                  alt="Cookpad Logo"
+                  class="mask mask-squircle"
+                />
+              </div>
+            </div>
+            <div class="flex-1">
+              <div class="text-lg font-extrabold">Cookpad</div>
+              <div class="text-base-content/70 text-sm">
+                Software Engineer – Internship
+              </div>
+              <div class="text-base-content/70 text-sm">
+                August 2022 – September 2022
+              </div>
+              <div class="text-base-content/70 text-sm">Kanagawa, Japan</div>
             </div>
           </div>
-          <div class="flex-1">
-            <div class="text-lg font-extrabold">Cookpad</div>
-            <div class="text-base-content/70 text-sm">
-              Software Engineer – Internship
-            </div>
-            <div class="text-base-content/70 text-sm">
-              August 2022 – September 2022
-            </div>
-            <div class="text-base-content/70 text-sm">Kanagawa, Japan</div>
+          <hr class="my-3" />
+          <div class="prose text-sm mb-3">
+            <strong>Responsibility:</strong>
+            <p>
+              Discovery of user issues, planning, development, and post-release
+              verification.
+            </p>
+            <strong>Achievement:</strong>
+            <ul class="list-disc list-inside">
+              <li>
+                Collaborated with the backend team and improved the UI/UX of the
+                Cookpad iOS app.
+              </li>
+            </ul>
           </div>
-        </div>
-        <hr class="my-3" />
-        <div class="prose text-sm mb-3">
-          <strong>Responsibility:</strong>
-          <p>
-            Discovery of user issues, planning, development, and post-release
-            verification.
-          </p>
-          <strong>Achievement:</strong>
-          <ul class="list-disc list-inside">
-            <li>
-              Collaborated with the backend team and improved the UI/UX of the
-              Cookpad iOS app.
-            </li>
-          </ul>
-        </div>
-        <div>
-          <div class="badge badge-ghost mr-1">iOS</div>
-          <div class="badge badge-ghost mr-1">Swift</div>
-          <div class="badge badge-ghost mr-1">Swift UI</div>
-          <div class="badge badge-ghost mr-1">UI Kit</div>
+          <div>
+            <div class="badge badge-ghost mr-1">iOS</div>
+            <div class="badge badge-ghost mr-1">Swift</div>
+            <div class="badge badge-ghost mr-1">Swift UI</div>
+            <div class="badge badge-ghost mr-1">UI Kit</div>
+          </div>
         </div>
       </div>
       <!-- End of Employment -->
       <h2>Education</h2>
 
-      <div
-        class="not-prose bg-base-100 rounded-box w-full max-w-lg shadow-xl p-4 pt-4 pb-4 mb-3"
-      >
-        <div class="flex gap-4 my-2">
-          <div class="avatar flex-none">
-            <div
-              class="mask mask-square bg-base-content h-24 w-24 bg-opacity-10"
-            >
-              <img
-                src="https://media.licdn.com/dms/image/C4E0BAQEGCJG4ukfFyA/company-logo_200_200/0/1519892728945?e=1683158400&v=beta&t=C8E1K-0B4AFnSY6whqjoOCvUSd0r0Srzrcr2lacnFUQ"
-                width="94"
-                height="94"
-                alt="TokyoTech Logo"
-                class="mask mask-square"
-              />
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div
+          class="not-prose bg-base-100 rounded-box w-full shadow-xl p-4 pt-4 pb-4"
+        >
+          <div class="flex gap-4 my-2 items-center">
+            <div class="avatar flex-none">
+              <div
+                class="mask mask-square bg-base-content h-24 w-24 bg-opacity-10"
+              >
+                <img
+                  src="https://media.licdn.com/dms/image/C4E0BAQEGCJG4ukfFyA/company-logo_200_200/0/1519892728945?e=1683158400&v=beta&t=C8E1K-0B4AFnSY6whqjoOCvUSd0r0Srzrcr2lacnFUQ"
+                  width="94"
+                  height="94"
+                  alt="TokyoTech Logo"
+                  class="mask mask-square"
+                />
+              </div>
             </div>
-          </div>
-          <div class="flex-1">
-            <div class="text-lg font-extrabold">
-              Tokyo Institute of Technology
+            <div class="flex-1">
+              <div class="text-lg font-extrabold">
+                Tokyo Institute of Technology
+              </div>
+              <div class="text-base-content/70 text-sm">
+                Master's Degree, Department of Computer Science
+              </div>
+              <div class="text-base-content/70 text-sm">
+                April 2022 – Present
+              </div>
+              <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
             </div>
-            <div class="text-base-content/70 text-sm">
-              Master's Degree, Department of Computer Science
-            </div>
-            <div class="text-base-content/70 text-sm">April 2022 – Present</div>
-            <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
           </div>
         </div>
-      </div>
 
-      <div
-        class="not-prose bg-base-100 rounded-box w-full max-w-lg shadow-xl p-4 pt-4 pb-4 mb-3"
-      >
-        <div class="flex gap-4 my-2">
-          <div class="avatar flex-none">
-            <div
-              class="mask mask-square bg-base-content h-24 w-24 bg-opacity-10"
-            >
-              <img
-                src="https://media.licdn.com/dms/image/C4E0BAQEGCJG4ukfFyA/company-logo_200_200/0/1519892728945?e=1683158400&v=beta&t=C8E1K-0B4AFnSY6whqjoOCvUSd0r0Srzrcr2lacnFUQ"
-                width="94"
-                height="94"
-                alt="TokyoTech Logo"
-                class="mask mask-square"
-              />
+        <div
+          class="not-prose bg-base-100 rounded-box w-full shadow-xl p-4 pt-4 pb-4"
+        >
+          <div class="flex gap-4 my-2 items-center">
+            <div class="avatar flex-none">
+              <div
+                class="mask mask-square bg-base-content h-24 w-24 bg-opacity-10"
+              >
+                <img
+                  src="https://media.licdn.com/dms/image/C4E0BAQEGCJG4ukfFyA/company-logo_200_200/0/1519892728945?e=1683158400&v=beta&t=C8E1K-0B4AFnSY6whqjoOCvUSd0r0Srzrcr2lacnFUQ"
+                  width="94"
+                  height="94"
+                  alt="TokyoTech Logo"
+                  class="mask mask-square"
+                />
+              </div>
             </div>
-          </div>
-          <div class="flex-1">
-            <div class="text-lg font-extrabold">
-              Tokyo Institute of Technology
+            <div class="flex-1">
+              <div class="text-lg font-extrabold">
+                Tokyo Institute of Technology
+              </div>
+              <div class="text-base-content/70 text-sm">
+                Bachelor's Degree, Department of Computer Science
+              </div>
+              <div class="text-base-content/70 text-sm">
+                April 2018 – March 2022
+              </div>
+              <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
             </div>
-            <div class="text-base-content/70 text-sm">
-              Bachelor's Degree, Department of Computer Science
-            </div>
-            <div class="text-base-content/70 text-sm">
-              April 2018 – March 2022
-            </div>
-            <div class="text-base-content/70 text-sm">Tokyo, Japan</div>
           </div>
         </div>
       </div>
