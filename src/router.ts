ad286f4c2d897d522d70b87764ffae733a9ce622
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-04-15 22:18:53 +0900

    Use createWebHashHistory to prevent 404

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./pages/Home.vue";
 import Portfolio from "./pages/Portfolio.vue";
 import Skills from "./pages/Skills.vue";
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
